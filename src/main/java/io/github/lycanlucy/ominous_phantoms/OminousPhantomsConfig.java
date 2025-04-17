0eea7972c6e96062420814e9d64d6f716f016c21
lycanlucy <lycanlucy@outlook.com>
2025-04-17 12:06:38 -0300

    1.0.1

--- src/main/java/io/github/lycanlucy/ominous_phantoms/OminousPhantomsConfig.java
+++ src/main/java/io/github/lycanlucy/ominous_phantoms/OminousPhantomsConfig.java
@@ -12,11 +12,17 @@ public class OminousPhantomsConfig {
             .comment("Whether Phantoms should drop have a chance of dropping bones on top of their standard loot table.")
             .translation("config.ominous_phantoms.phantoms_drop_bones")
             .define("phantomsDropBones", true);
+    private static final ModConfigSpec.BooleanValue PHANTOMS_ONLY_ATTACK_AFFLICTED_PLAYERS = BUILDER
+            .comment("Whether Phantoms should only attack players with the Night Omen effect.")
+            .translation("config.ominous_phantoms.phantoms_only_attack_afflicted_players")
+            .define("phantomsOnlyAttackAfflictedPlayers", true);
     static final ModConfigSpec SPEC = BUILDER.build();
     public static boolean phantomsDropBones;
+    public static boolean phantomsOnlyAttackAfflictedPlayers;
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
         phantomsDropBones = PHANTOMS_DROP_BONES.get();
+        phantomsOnlyAttackAfflictedPlayers = PHANTOMS_ONLY_ATTACK_AFFLICTED_PLAYERS.get();
     }
 }
---
+++ src/main/java/io/github/lycanlucy/ominous_phantoms/mixin/PhantomAttackPlayerTargetGoalMixin.java
@@ -0,0 +1,44 @@
+package io.github.lycanlucy.ominous_phantoms.mixin;
+
+import io.github.lycanlucy.ominous_phantoms.OminousPhantomsConfig;
+import io.github.lycanlucy.ominous_phantoms.effect.OminousPhantomsEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.monster.Phantom;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Mixin(targets = "net.minecraft.world.entity.monster.Phantom$PhantomAttackPlayerTargetGoal")
+public abstract class PhantomAttackPlayerTargetGoalMixin {
+    @Shadow @Final
+    Phantom this$0;
+    @Unique
+    private final TargetingConditions ominous_phantoms$pickyAttackTargeting = TargetingConditions.forCombat().selector(livingEntity -> livingEntity.hasEffect(OminousPhantomsEffects.NIGHT_OMEN)).range(64.0F);
+
+    @Inject(method = "canUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/Phantom$PhantomAttackPlayerTargetGoal;reducedTickDelay(I)I", shift = At.Shift.AFTER), cancellable = true)
+    private void canUse(CallbackInfoReturnable<Boolean> cir) {
+        if (OminousPhantomsConfig.phantomsOnlyAttackAfflictedPlayers) {
+            List<Player> list = this$0.level().getNearbyPlayers(this.ominous_phantoms$pickyAttackTargeting, this$0, this$0.getBoundingBox().inflate(16.0F, 64.0F, 16.0F));
+            if (!list.isEmpty()) {
+                list.sort(Comparator.<Entity, Double>comparing(Entity::getY).reversed());
+
+                for(Player player : list) {
+                    if (this$0.canAttack(player, TargetingConditions.DEFAULT)) {
+                        this$0.setTarget(player);
+                        cir.setReturnValue(true);
+                    }
+                }
+            }
+            cir.setReturnValue(false);
+        }
+    }
+}
